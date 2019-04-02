c84172e5f9eafeb56db089779477226a2dc0445a
unknown <nunof.h.fontes@gmail.com>
2019-04-02 23:36:27 +0100

    schemes added for user, portfolio and quotes

--- models/quote.model.js
+++ models/quote.model.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let QuoteSchema = new Schema({
+let QuotePortfolio = new Schema({
     name: {type: String, required: true, max: 100},
     price: {type: Number, required: true},
 });
 
+let QuotePortfolio = new Schema({
+    name: {type: String, required: true, max: 100},
+    price: {type: Number, required: true},
+});
+
+let QuoteSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    price: {type: Number, required: true},
+});
 
 // Export the model
 module.exports = mongoose.model('Quote', QuoteSchema);
