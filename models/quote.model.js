4bd3cb7d948619eb80ad9dc76d719b75c5e2d2b5
unknown <nunof.h.fontes@gmail.com>
2019-04-03 19:20:09 +0100

    service response example added and quote schema updated, but incomplete

--- models/quote.model.js
+++ models/quote.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let QuotePortfolio = new Schema({
+let UserSchema = new Schema({
     name: {type: String, required: true, max: 100},
-    price: {type: Number, required: true},
+    symbols: {type: Array, required: false},
+    portofolioId: {type: Number, required: true},
 });
 
-let QuotePortfolio = new Schema({
+/* let QuotePortfolioSchema = new Schema({
     name: {type: String, required: true, max: 100},
     price: {type: Number, required: true},
-});
+}); */
 
 let QuoteSchema = new Schema({
+    symbol: {type: String, required: true, max: 100},
     name: {type: String, required: true, max: 100},
     price: {type: Number, required: true},
 });
 
 // Export the model
 module.exports = mongoose.model('Quote', QuoteSchema);
+module.exports = mongoose.model('User', UserSchema);
