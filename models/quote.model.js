54988821fc23833de29177c3f7a1d6f07c62315c
unknown <nunof.h.fontes@gmail.com>
2019-04-07 00:41:39 +0100

    adding schema for Data Model Example 3

--- models/quote.model.js
+++ models/quote.model.js
@@ -4,13 +4,32 @@ const Schema = mongoose.Schema;
 let QuoteSchema = new Schema({
 
     //a brief info about the security
-    symbol: {type: String, required: true, max: 100},
-    name: {type: String, required: true, max: 100},
+    symbol: {type: String, required: true},
+    name: {type: String, required: false},
     price: {type: Number, required: true},
 
     //insert the entire service's response here
 
 });
+var Quote = mongoose.model('Quote', QuoteSchema);
+
+let EquitySchema = new Schema({
+    equity: {type: Array, required: false}
+});
+var Equity = mongoose.model('Equity', EquitySchema);
+
+
 
 // Export the model
-module.exports = mongoose.model('Quote', QuoteSchema);
+//module.exports = mongoose.model('Quote', QuoteSchema);
+
+module.exports = {
+    Quote: Quote,
+    Equity: Equity
+
+    /*
+    Answer: Answer,
+    Module: Module,
+    Role: Role
+    */
+};
