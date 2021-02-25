d4aa65ae212782bf06e1fee35dcdd62e5a9f833c
nunofontes <nunof.h.fontes@gmail.com>
2021-02-25 19:30:42 +0000

    test commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+console.log('routes - index.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
