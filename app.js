0e7a2d0074de73925bf2c48c7e0e268b4e70ea9e
nunofontes <nunof.h.fontes@gmail.com>
2021-02-28 13:42:04 +0000

    added HTTP headers and Quotes controller

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// require("dotenv-safe").config();
+// const jwt = require('jsonwebtoken');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var quotesRouter = require('./routes/quote.route'); // Imports routes 
+const http = require('http');
 
-var app = express();
+let createError = require('http-errors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-console.log("running app.js");
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let quotesRouter = require('./routes/quote.route'); // Imports routes 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//creating Express app
+const express = require('express');
+const app = express();
+
+//body parser to read JSON from request's 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//setting HTTP Headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Loading the severall routers here, treat each url in the proper router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/quotes', quotesRouter);
 
+// testing
+app.get('/clientes', (req, res, next) => { 
+  console.log("Retornou todos clientes!");
+  res.json([{id:1,nome:'luiz'}]);
+}) 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,24 +63,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-///////////////////////////////TESTING AREA/////////////////////////////////////////////////////
-/*
-const fs = require('fs');
 
-//let rawdata = fs.readFileSync('./models/example3.json');  
-//let example3 = JSON.parse(rawdata);  
-//console.log(example3);  
+//Build a server to intercept requests
+const server = http.createServer(app); 
+server.listen(3000);
+console.log("Listening on port 3000...")
 
-console.log('This is before the read call');  
-
-fs.readFile('./models/example3.json', (err, data) => {  
-  if (err) throw err;
-  let example3 = JSON.parse(data);
-  console.log(example3);
-});
-
-console.log('This is after the read call');  
-*/
-////////////////////////////////////////////////////////////////////////////////////////////////
 
 module.exports = app;
