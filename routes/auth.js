a7998bd1f35bad35357c42e54060e707a7868f84
nunofontes <nunof.h.fontes@gmail.com>
2021-03-01 21:43:16 +0000

    auth middleware TODO changes

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   let decodedToken;
   try {
+      //Consider using jwt.verify(token, process.env.SECRET -> the .env file
     decodedToken = jwt.verify(token, 'somesupersecretsecret');
   } catch (err) {
     err.statusCode = 500;
@@ -20,6 +21,8 @@ module.exports = (req, res, next) => {
     error.statusCode = 401;
     throw error;
   }
+
+  //if everything goes well, save the id on the request
   req.userId = decodedToken.userId;
   next();
 };
