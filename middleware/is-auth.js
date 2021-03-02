87ee8e3d4e4019416f47565edda5e9f92bf914f1
nunofontes <nunof.h.fontes@gmail.com>
2021-03-02 22:05:50 +0000

    added middleware to check authentication

---
+++ middleware/is-auth.js
@@ -0,0 +1,29 @@
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+  try {
+      //Consider using jwt.verify(token, process.env.SECRET -> the .env file
+    decodedToken = jwt.verify(token, process.env.SECRET);
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+
+  //if everything goes well, save the id on the request
+  req.userId = decodedToken.userId;
+  next();
+};
