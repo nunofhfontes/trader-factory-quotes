950c3a07f7bde72657ae1b57f44e4a66b42d5d2a
nunofontes <nunof.h.fontes@gmail.com>
2021-03-03 23:20:56 +0000

    New User schema

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ console.log('routes - users.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({'message': 'respond with a resource'});
 });
 
 module.exports = router;
