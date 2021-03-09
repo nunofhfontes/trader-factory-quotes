ae4c9658f862f1dc697eb7d97ce26e747011ef0d
nunofontes <nunof.h.fontes@gmail.com>
2021-03-09 22:36:08 +0000

    added portfolio schema

---
+++ models/portfolio.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const portfolioSchema = new Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  createDate: {
+    type: String,
+    required: true
+  },
+  lastModified: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'I am new!'
+  },
+  securities: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Stock'
+    }
+  ]
+});
+
+module.exports = mongoose.model('Portfolio', portfolioSchema);
