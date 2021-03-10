ab468d70e648029b8568dba6d37851fa4d46ecf0
nunofontes <nunof.h.fontes@gmail.com>
2021-03-10 22:21:19 +0000

    Portfolio TODO list update

--- controllers/portfolio.controller.js
+++ controllers/portfolio.controller.js
@@ -2,4 +2,22 @@
 
 exports.getPortfolio = (req, res, next) => {
 
+    //get the portfolio by the user stored in the jwt
+
+};
+
+exports.getPortfolioByType = (req, res, next) => {
+
+    //get the portfolio by the user stored in the jwt - TYPE
+
+};
+
+exports.add2Portfolio = (req, res, next) => {
+
+
+};
+
+exports.delFromPortfolio = (req, res, next) => {
+
+
 };
