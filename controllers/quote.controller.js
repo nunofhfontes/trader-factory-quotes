7cb41069cc50dc1659796f3227e5c8f4a21d8b47
nunofontes <nunof.h.fontes@gmail.com>
2021-03-10 22:23:41 +0000

    reused API key and http call to get Stock Quote

--- controllers/quote.controller.js
+++ controllers/quote.controller.js
@@ -1,6 +1,20 @@
+var request = require('request');
 const Quote = require('../models/quote.model');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
     res.send('Greetings from the Quotes Test controller!');
 };
+
+
+//Get stock quotes for symbol X
+request(stockQuotesApiUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        //console.log(body) // Print the google web page.
+        var apiResponse = body;
+        console.log('typeof ----->> ', typeof apiResponse);
+        console.log(apiResponse);
+    } else {
+        console.log("ERROR");
+    }
+})
